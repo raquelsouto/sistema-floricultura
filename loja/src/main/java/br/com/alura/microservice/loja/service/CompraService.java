eb1d1597224fb967e9d87b4f8673714d84eac3c2
Raquel Souto <kelsouto@gmail.com>
2021-07-12 20:05:52 -0300

    Implementação do LoadBalance

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -3,6 +3,7 @@ package br.com.alura.microservice.loja.service;
 import br.com.alura.microservice.loja.dto.CompraDTO;
 import br.com.alura.microservice.loja.dto.InfoFornecedorDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -14,11 +15,20 @@ public class CompraService {
     @Autowired
     private RestTemplate cliente;
 
-    public void realizaCompra(CompraDTO compraDTO) {
-        RestTemplate cliente = new RestTemplate();
+    @Autowired
+    DiscoveryClient eurekaClient;
 
+    public void realizaCompra(CompraDTO compraDTO) {
         ResponseEntity<InfoFornecedorDTO> exchange = cliente.exchange("http://fornecedor/info/"+compraDTO.getEndereco().getEstado(),
                 HttpMethod.GET, null, InfoFornecedorDTO.class);
+
+
+        /*Pega as informações do eurekaClient, substitui o ID de fornecedor por IP e PORTA da instância e
+        seleciona para onde a requisição será enviada*/
+        eurekaClient.getInstances("fornecedor").stream()
+                        .forEach(fornecedor -> {
+                            System.out.println("localhost:"+fornecedor.getPort());
+                        }) ;
 
         System.out.println(exchange.getBody().getEndereco());
 
