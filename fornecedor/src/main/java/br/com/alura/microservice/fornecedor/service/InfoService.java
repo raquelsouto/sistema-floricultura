edb6e55050fda477c77a182d218eef4e1701eef4
Raquel Souto <kelsouto@gmail.com>
2021-07-11 23:07:21 -0300

    Implementação do Eureka server

--- fornecedor/src/main/java/br/com/alura/microservice/fornecedor/service/InfoService.java
+++ fornecedor/src/main/java/br/com/alura/microservice/fornecedor/service/InfoService.java
@@ -1,7 +1,6 @@
 package br.com.alura.microservice.fornecedor.service;
 
 import br.com.alura.microservice.fornecedor.model.InfoFornecedor;
-import br.com.alura.microservice.fornecedor.repository.InfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
--- loja/src/main/java/br/com/alura/microservice/loja/controller/CompraController.java
+++ loja/src/main/java/br/com/alura/microservice/loja/controller/CompraController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/compra")
+@RequestMapping("/compras")
 public class CompraController {
 
     @Autowired
